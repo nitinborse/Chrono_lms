27e08faef5b09870325a9aed3886a32b9c013eb7
nitinborse <57225506+nitinborse@users.noreply.github.com>
2025-06-02 12:06:16 +0530

    Update Register.js

--- super-teacher-frontend/src/Register.js
+++ super-teacher-frontend/src/Register.js
@@ -28,7 +28,7 @@ export default function Register({ switchToLogin }) {
     setSuccess("");
 
     try {
-      const res = await fetch("http://localhost:5000/register", {
+      const res = await fetch("https://chrono-lms.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
